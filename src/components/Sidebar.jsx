b72b6db7184f7e05b78a8e8a0a5ad4b57f8d7137
Allan Paulo de Santana <allanxlink@hotmail.com>
2023-04-04 10:58:59 -0300

    update files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import Avatar from '../img/allan.jpg'
 import '../styles/components/sidebar.sass'
+import SocialNeteorks from './SocialNetworks'
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Allan Paulo" />
       <p className="title">Desenvolvedor</p>
-      <p>redes sociais</p>
+      <SocialNeteorks />
       <p>informações de contato</p>
       <a href="#" className="btn">Download currículo</a>
     </aside>
